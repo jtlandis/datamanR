6bcd5bf86d241225fbba09973de2a0160a4efd20
jtlandis <jtlandis314@gmail.com>
2020-09-17 22:02:46 -0400

    Making more definitions

--- R/Class_Definitions.R
+++ R/Class_Definitions.R
@@ -1,40 +1,6 @@
 
 
-#' @title TableDef
-#' @name TableDef
-#' @description Prototype object that holds the defining factors
-#' of a Data Table. This object is of class "def" including class
-#' "proto" and "environment". Use this class to define the constraints
-#' and characteristics of a Data Table.
-#' @section Feilds:
-#'
-#' The fields specify attributes of a Data Table. Not all fields are
-#' necessary, but those in bold should be defined. The associated set
-#' method is displayed in which it takes one argument, its associated
-#' field. Most set methods are just convienice and help standardize
-#' field expectations. Not all fields have a set method as they are
-#' updated by another Method. Most fields have an assocated valid{Field}
-#' method that simply checks if the associated field is NA or not.
-#'
-#' \itemize{
-#' \item{data}{ \code{setTableData(data)}.
-#' holds the Data Table in memory. This field is not necessary,
-#' but is a convience for programming.setMethod: setTableData}
-#' \item{\strong{name}}{ \code{setTableName(name)}.
-#' holds the character string that defines the name of the Data
-#' Table. Each Data Table name should be unique within a Data Manager.}
-#' \item{\strong{file}}{ \code{setTableFile(file)}. holds the full path to a file where the data will be stored
-#' on disk.}
-#' \item{\strong{col_names}}{ \code{setTableColnames(col_names)}. Specifies the Column names of the Data Table.}
-#' \item{\strong{col_types}}{ \code{setTableColtypes(col_types)}. Specifies the Column classes of the Data Table.}
-#' \item{keys}{ \code{setTableKeys(keys)}. Specifies which Columns are keys. Keys are not necessary,
-#' but a convience. In some Data Table designs, a set of keys may express
-#' a set of columns that uniquely define each row within a Data Table
-#' and/or express which keys may be joined with other Data Tables in
-#' a Data Manager.}
-#' \item{md5sum}{ Stores the md5sum that is stored in the file field. md5sum should not be set, instead it is updated with \code{updateClass()} method.}
-#'}
-#'
+
 #'
 #' Methods to set each field
 #' @export
@@ -94,6 +60,42 @@ BaseDMR <- R6::R6Class(classname = "BaseDMR",
                             )
                        )
 
+#' @title TableDef
+#' @name TableDef
+#' @description Prototype object that holds the defining factors
+#' of a Data Table. This object is of class "def" including class
+#' "proto" and "environment". Use this class to define the constraints
+#' and characteristics of a Data Table.
+#' @section Feilds:
+#'
+#' The fields specify attributes of a Data Table. Not all fields are
+#' necessary, but those in bold should be defined. The associated set
+#' method is displayed in which it takes one argument, its associated
+#' field. Most set methods are just convienice and help standardize
+#' field expectations. Not all fields have a set method as they are
+#' updated by another Method. Most fields have an assocated valid{Field}
+#' method that simply checks if the associated field is NA or not.
+#'
+#' \itemize{
+#' \item{data}{ \code{setTableData(data)}.
+#' holds the Data Table in memory. This field is not necessary,
+#' but is a convience for programming.setMethod: setTableData}
+#' \item{\strong{name}}{ \code{setTableName(name)}.
+#' holds the character string that defines the name of the Data
+#' Table. Each Data Table name should be unique within a Data Manager.}
+#' \item{\strong{file}}{ \code{setTableFile(file)}. holds the full path to a file where the data will be stored
+#' on disk.}
+#' \item{\strong{col_names}}{ \code{setTableColnames(col_names)}. Specifies the Column names of the Data Table.}
+#' \item{\strong{col_types}}{ \code{setTableColtypes(col_types)}. Specifies the Column classes of the Data Table.}
+#' \item{keys}{ \code{setTableKeys(keys)}. Specifies which Columns are keys. Keys are not necessary,
+#' but a convience. In some Data Table designs, a set of keys may express
+#' a set of columns that uniquely define each row within a Data Table
+#' and/or express which keys may be joined with other Data Tables in
+#' a Data Manager.}
+#' \item{md5sum}{ Stores the md5sum that is stored in the file field. md5sum should not be set, instead it is updated with \code{updateClass()} method.}
+#'}
+#'
+#' @export
 TableDefinition <- R6::R6Class(classname = "TableDefinition",
                                inherit = BaseDMR,
                                private = list(
@@ -134,9 +136,7 @@ TableDefinition <- R6::R6Class(classname = "TableDefinition",
                                    if(missing(value)){
                                      private$.md5sum
                                    } else {
-                                     require_scalar_char(value, "`$md5sum`")
-                                     private$.md5sum <- value
-                                     self
+                                     abort("`$md5sum` is read only. Value depends on disk image of `$data`.")
                                    }
                                  }
                                  ),
@@ -155,7 +155,6 @@ TableDefinition <- R6::R6Class(classname = "TableDefinition",
                                    self$col_names <- col_names %||% NA_character_
                                    self$col_types <- col_types %||% NA_character_
                                    self$keys <- keys
-                                   self$md5sum <- md5sum
                                  },
                                  print = function(){
                                    cat("DataTableDefinitions:\n",
@@ -173,105 +172,65 @@ TableDefinition <- R6::R6Class(classname = "TableDefinition",
                                      need2(file.exists(self$path), glue("file: {self$path} does not exist yet!"))
                                    )
                                    validate_error(
-                                     need2(!is.na(self$name), glue("`$name` is not defined: {self$name}")),
-                                     need2(!is.na(self$path), glue("`$path` is not defined: {self$path}")),
-                                     need2(dir.exists(dirname(self$path)), glue("directory: {dirname(self$path)} does not exist yet!")),
+                                     need2(!is.na(self$name),
+                                           glue("`$name` is not defined: {self$name}")),
+                                     need2(!is.na(self$path),
+                                           glue("`$path` is not defined: {self$path}")),
+                                     need2(dir.exists(dirname(self$path)),
+                                           glue("directory: {dirname(self$path)} does not exist yet!")),
                                      need2(!is.null(self$data), "No data is loaded"),
-                                     need2(all(self$col_names == colnames(self$data)), "`$col_names` do not match data Column names."),
-                                     need2(all(self$col_types == colclasses(self$data)), "`$col_types` do not match classes of data."),
+                                     need2(all(self$col_names == colnames(self$data)),
+                                           "`$col_names` do not match data Column names."),
+                                     need2(all(self$col_types == colclasses(self$data)),
+                                           "`$col_types` do not match classes of data."),
                                      need2(all(self$keys %in% self$col_names),
                                           glue("The following `$keys` are not a subset of `$col_names`:\n",
                                                "  {paste(self$keys[!self$keys%in%self$col_names], collapse = \", \")}"))
                                    )
                                    TRUE
+                                 },
+                                 update = function(){
+                                   #if data has changed, update col_names and col_types
+                                   if(is.null(self$data)){
+                                     warn("`$data` is NULL. Please provide data before updating")
+                                   } else {
+                                     self$col_names <- colnames(self$data)
+                                     self$col_types <- colclasses(self$data)
+                                   }
+                                   if(file.exists(self$path)){
+                                     self$md5sum <- tools::md5sum(self$path)
+                                   } else {
+                                     warn("`$path` is not exist. Please write table to update md5sum.")
+                                   }
+                                   self
+                                 }
+                                 write = function(data = self$data,
+                                                  file = self$path,
+                                                  sep = ",",
+                                                  append = F){
+                                   file <- full_path(file)
+                                   data.table::fwrite(x = data, file = file, sep = sep, append = append)
+                                   private$md5sum <- tools::md5sum(files = file)
+                                 },
+                                 read = function() {
+                                   if(file.exists(self$path)){
+                                     data_table <- data.table::fread(file = self$file, header = T, colClasses = self$col_types)
+                                   } else if(!any(is.na(self$col_names))&&!any(is.na(self$col_types))){
+                                     warn(glue("file: {self$path} is not valid.\nloading empty data.table.\n"))
+                                     data_table <- data.table::fread(c(paste0(self$col_names, collapse = ","),"\n"), colClasses = self$col_types)
+                                   } else {
+                                     error <- c()
+                                     rlang::abort(glue("Cannot read table from definition.\n",
+                                                       "file: {self$path} is not valid.\n",
+                                                       "No defined Column Names or Column Classes.\n"))
+                                   }
+                                   return(data_table)
+                                 },
+                                 save = function(location = dirname(self$path), name = paste0(self$name, "_tableDef.rds")){
+                                   saveRDS(object = self, file = paste0(location,"/", name))
                                  }
                                ))
 
-  data = NULL,
-                  name = NA_character_,
-                  file = NA_character_,
-                  col_names = NA_character_,
-                  col_types = NA_character_,
-                  keys = NA_character_,
-                  md5sum = NA_character_,
-                  setTableData = function(., data = .$data) {.$data <- data },
-                  setTableName = function(., name = .$name) {.$name <- name },
-                  setTableFile = function(., file = .$file) {.$file <- file %na% full_path(file) },
-                  setTableColnames = function(., col_names = .$col_names) {.$col_names <- col_names},
-                  setTableColtypes = function(., col_types = .$col_types) {.$col_types <- col_types},
-                  setTableKeys = function(., keys = .$keys) {.$keys <- keys },
-                  validName = function(.) !is.na(.$name),
-                  validFile = function(.) !is.na(.$file)&&file.exists(.$file),
-                  validData = function(.) !(is.null(.$data)),
-                  validColnames = function(.) !any(is.na(.$col_names)),
-                  validColtypes = function(.) !any(is.na(.$col_types)),
-                  write_table = function(.,
-                                         data = .$data,
-                                         file = .$file,
-                                         sep = ",",
-                                         append = F){
-                     file <- full_path(file)
-                     data.table::fwrite(x = data, file = file, sep = sep, append = append)
-                   },
-                  read_table = function(.){
-                    if(.$validFile()){
-                      data_table <- data.table::fread(file = .$file, header = T, colClasses = .$col_types)
-                    } else if(.$validColnames()&&.$validColtypes()){
-                      error <- "file: {.$file} is not valid\n"
-                      warn(glue(error, "loading empty data.table.\n"))
-                      data_table <- data.table::fread(c(paste0(.$col_names, collapse = ","),"\n"), colClasses = .$col_types)
-                    } else {
-                      error <- c("file: {.$file} is not valid.\nNo defined Column Names or Column Classes.\n")
-                      rlang::abort(glue("Cannot read table from definition.\n", error))
-                    }
-                    return(data_table)
-                  },
-                  updateClass = function(.){ #updates standard imports to something more standard
-                    .$setTableFile()
-                    if(.$validData()){
-                      .$setTableColnames(colnames(.$data))
-                      .$setTableColtypes(column_classes(.$data))
-                    }
-                    if(.$validFile()){
-                      .$md5sum <- tools::md5sum(.$file)
-                    }
-                  },
-                  isValid = function(.){
-                    errors <- c()
-                    if(!.$validName()){
-                      errors <- c(errors, "Data Table is unnamed.")
-                    }
-                    if(is.na(.$file)){
-                      errors <- c(errors, glue("file is not defined."))
-                    } else if(!.$validFile()){
-                      rlang::warn(glue("file: {.$file} does not exist yet."))
-                    }
-                    if(!.$validColnames()) {
-                      errors <- c(errors, "Column Names are not defined.")
-                    }
-                    if(!.$validColtypes()) {
-                      errors <- c(errors, "Column Classes are not defined.")
-                    }
-                    if(length(errors)>0){
-                      rlang::abort(glue_collapse(errors, sep = "\n"))
-                    }
-                  },
-                  new = function(.,
-                                    data = .$data,
-                                    name = .$name,
-                                    file = .$file,
-                                    col_names = .$col_names,
-                                    col_types = .$col_types,
-                                    keys = .$keys) {
-                    tmp <- .$proto(data = data, name = name, file = file, col_names = col_names, col_types = col_types, keys = keys)
-                    tmp$updateClass()
-                    tmp$isValid()
-                    class(tmp) <- c("def", class(tmp))
-                    return(tmp)
-                  },
-                  save = function(., location){
-                    saveRDS(., file = paste0(location,"/",.$name,"_tableDef.rds"))
-                  })
 
 #' @title TableDef Methods
 #' @name TableDef_methods
