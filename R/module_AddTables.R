da5baebcf7f0a96a21658b3e83d32b91efb3efff
Justin Landis <jtlandis@live.unc.edu>
2020-09-23 17:50:35 -0400

    can add ui elements as intended. Need to be able to retrieve inputs out.

--- R/module_AddTables.R
+++ R/module_AddTables.R
@@ -1,6 +1,6 @@
 
 
-
+#' @title addDefUI
 #' @export
 addDefUI <- function(id){
   ns <- NS(id)
@@ -12,8 +12,8 @@ addDefUI <- function(id){
       tabPanel("Read  Remote",
                readremoteUI(ns("table_remote"))
                ),
-      tabPanel("Define New"#,
-               # definetablUI(ns("table_define"))
+      tabPanel("Define New",
+               definetablUI(ns("table_define"))
                )
     ),
     hr(),
@@ -36,22 +36,22 @@ addDefServer <- function(id, roots = c(home = getwd())){
       )
       upload_def <- uploadfileServer(id = "table_upload", roots = roots)
       remote_def <- readremoteServer(id = "table_remote", roots = roots)
-      #define_def <- definetablServer(id = "table_define")
+      define_def <- definetablServer(id = "table_define", roots = roots)
 
       observe({
         upload_def$set()
         deftab$data <- upload_def$data()
         deftab$name <- upload_def$name()
-        deftab$path <- upload_def$path()
         deftab$keys <- upload_def$keys()
+        deftab$path <- upload_def$path()
       })
 
       observe({
         remote_def$set()
         deftab$data <- remote_def$data()
         deftab$name <- remote_def$name()
-        deftab$path <- remote_def$path()
         deftab$keys <- remote_def$keys()
+        deftab$path <- remote_def$path()
       })
 
       output$preview <- renderPrint({
@@ -460,19 +460,178 @@ readremoteServer <- function(id, roots = c(home = getwd())){
 }
 
 
-
+#' @export
 definetablUI <- function(id){
   ns <- NS(id)
-  verbatimTextOutput(ns("tbd"), "TBD")
+  tagList(
+    h2("Define Table Information:"),
+    fluidRow(
+      column(3,
+             textInput(ns("name"), "Table Name:")),
+      column(3,
+             textInput(ns("file_name"), "File Name:")),
+      column(2,
+             div(shinyDirButton(ns("directory"), "location", "Choose A Directory"), align = "center")),
+      column(4,
+             htmlOutput(ns("dirout")))
+    ),
+    fluidRow(
+      column(12,
+             uiOutput(ns("making")))
+    ),
+    fluidRow(
+      column(12,
+             tags$div(id="ele_indx_0")
+             )
+    )
+  )
 }
 
-definetablServer <- function(id){
+#' @export
+definetablServer <- function(id, roots){
   moduleServer(
     id,
     function(input, output, session){
+
+      count <- reactiveVal(0, "Count")
+      nele <- reactiveVal(0, "N_ele")
+      where <- reactiveVal("afterEnd", "insert_where")
+      selec <- reactiveVal("#ele_indx_0")
+
+      shinyDirChoose(input, id = "directory", roots = roots)
+      nam <- reactive({
+        validate(need(input$name, "Please provide a name for the Table."))
+        input$name
+      })
+
+      file <- reactive({
+        validate(need(input$file_name, "Please provide a descriptive name for disk."))
+        input$file_name
+      })
+
+      dir <- reactive({
+        validate(
+          need(input$directory, "Please select a directory to which files will be saved."),
+          need(!is.null(input$directory), "directory button is null. Try again")
+        )
+        parseDirPath(roots = roots, input$directory)
+      })
+
+      output$dirout <- renderPrint(str_c(p(strong(dir()), style = "word-wrap: break-word;")))
+
+
+
+      output$making <- renderUI({
+        ns <- session$ns
+        if(nele()<=0){
+          tagList(
+            div(actionButton(ns("add"), "Start"), align = "center")
+          )
+        } else {
+          tagList(h3(str_c("There are ", nele(), " potential columns.")))
+        }
+      })
+
+      observeEvent(input$add, {
+        req(input$add)
+        count(count()+1)
+        nele(nele()+1)
+        })
+
+
+
+      observeEvent(count(), {
+        req(count()>0)
+        ns <- session$ns
+        select <- ifelse(nele()==1, "#ele_indx_0", selec())
+        id_add <- str_c("indx_", count())
+        remove_id <- str_c("remove_", id_add)
+        ele_id <- str_c("ele_", id_add)
+        insertUI(
+          selector = select,
+          where = where(),
+          ui= tags$div(
+            id = ele_id,
+
+            hr(),
+              fluidRow(
+
+                column(5,
+                       textInput(ns(str_c(id_add, ".colname")), label = NULL, placeholder = "New Column Name"), sytle = "max-height:25px;"),
+                column(3,
+                       selectInput(ns(str_c(id_add, ".coltype")),
+                                   label = NULL,
+                                   choices = c("character","numeric","integer","factor","logical"), selected = "character"), sytle = "max-height:25px;"),
+                column(2,
+                       checkboxInput(ns(str_c(id_add, ".key")), "Is key?", FALSE), sytle = "max-height:25px;"),
+                column(2,
+                       actionButton(ns(str_c(id_add, ".add_up")), NULL, icon = icon("arrow-up"), style = "background-color:#72B4D6;"),
+                       actionButton(ns(str_c(remove_id)), NULL, icon = icon("minus-square"), style = "background-color:#FF9270;"),
+                       actionButton(ns(str_c(id_add, ".add_bot")), NULL, icon = icon("arrow-down"), style = "background-color:#72B4D6;"), sytle = "max-height:25px;")
+              ), hr()
+          ))
+
+        observeEvent(input[[remove_id]],{
+          isolate(nele(nele()-1))
+          removeUI(
+            selector = str_c("#", ele_id)
+          )
+        })
+
+        observeEvent(input[[str_c(id_add,".add_up")]],{
+          isolate(where("beforeBegin"))
+          isolate(selec(str_c("#",ele_id)))
+          isolate(nele(nele()+1))
+          count(count()+1)
+        })
+
+        observeEvent(input[[str_c(id_add,".add_bot")]],{
+          isolate(where("afterEnd"))
+          isolate(selec(str_c("#",ele_id)))
+          isolate(nele(nele()+1))
+          count(count()+1)
+        })
+
+      })
+
+
+
 
     }
   )
 }
 
 
+# define_ui <- function(id){
+#   ns <- NS(id)
+#   tags$div(id = environment(ns)[['namespace']],
+#           tagList(
+#             fluidRow(
+#                      column(5,
+#                             textInput(ns("colname"), label = NULL, placeholder = "New Column Name")),
+#                      column(3,
+#                             selectInput(ns("coltype"),
+#                                         label = NULL,
+#                                         choices = c("character","numeric","integer","factor","logical"), selected = "character")),
+#                      column(2,
+#                             checkboxInput(ns("key"), "Is key?", FALSE)),
+#                      column(2,
+#                             fluidRow(actionButton(ns("add_up"), NULL, icon = icon("dashboard"), style = "height:15px")),
+#                             fluidRow(actionButton(ns("delete_this"), NULL, icon = icon("minus-squar"), style = "height:15px")),
+#                             fluidRow(actionButton(ns("add_bot"), NULL, icon = icon("arrow-from-top"), style = "height:15px")))
+#                      )
+#           )
+#   )
+# }
+#
+# define_server <- function(id){
+#
+# }
+#
+# t <- fluidPage(shiny::wellPanel(define_ui("test")))
+#
+# s <- function(input, output, session) {
+#
+# }
+#
+# shinyApp(t,s)
