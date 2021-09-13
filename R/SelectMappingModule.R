f2d1c07c62bea28810237d856882d7030ae3af24
Justin Landis <jtlandis@pop-os.localdomain>
2021-09-13 15:13:27 -0400

    The main module works with reactive `select_(left|right)` arguments.

--- R/SelectMappingModule.R
+++ R/SelectMappingModule.R
@@ -33,23 +33,7 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
         counter <<- x
         invisible(counter)
       }
-      reindex <- function() {
-        browser()
-
-      }
-
-      # observeEvent(depend(), {
-      #   browser()
-      #   context_local(id)
-      #   n <- length(selected_left())
-      #   ids <- seq_len(n)
-      #   set_mapping_index(ids)
-      #   if(n==0) {
-      #     mods(NULL)
-      #   } else {
-      #     mods(lapply(get_mapping_index(), map_pair_select_Server))
-      #   }
-      # }, priority = -5L)
+      removing <- 0L
 
 
       #Whenever the options change, the UI should be reindexed
@@ -57,25 +41,27 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
         choices_left()
         choices_right()
       }, {
-        browser()
+        removing <<- 0L
         context_local(id)
         for(i in get_mapping_index()) {
-          zap_ns <- glue("{ns(i)}")
-          remove_shiny_inputs(c(glue("{zap_ns}-left"),glue("{zap_ns}-right"),glue("{zap_ns}-rm")), .input = input)
-          removeUI(glue("#{zap_ns}-pair_select_container"))
           zap_mapping_index(i)
+          click(glue("{i}-rm"))
+          removing <<- removing + 1L
         }
         set_index()
-        mods(NULL)
-        depend(depend()+1L)
       }, priority = 5L)
 
+
+      #if there are any default values
+      #insert them when appropriate after
+      #the UI has been cleared
       observeEvent({
         depend()
       }, {
-        browser()
+        #browser()
         context_local(id)
         validate(
+          need(is_empty(mods()), "No Modules"),
           need(!is.null(choices_left())&!is.null(choices_right()), "No options assigned to either right or left."),
           need(all(selected_left() %in% choices_left()), "One or more elements assigned on the left side is not apart of available options."),
           need(all(selected_right() %in% choices_right()), "One or more elements assigned on the right side is not apart of available options."),
@@ -101,16 +87,7 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
       },
       priority = 2L)
 
-      # output$mapping <- renderUI({
-      #   browser()
-      #
-      #
-      #   mods(lapply(get_mapping_index(), map_pair_select_Server))
-      #
-      #
-      #   do.call('tagList', if(length(get_mapping_index())<=1) list(pair_list_ui) else pair_list_ui)
-      # })
-
+      # Interactively add new pair maps
       observeEvent(input$add,
                    {
                      context_local(id)
@@ -119,15 +96,16 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
                      ui <- map_pair_select_UI(ns(new_id),
                                               choices_left = choices_left(),
                                               choices_right = choices_right(),
-                                              selected_left = selected_left(),
-                                              selected_right = selected_right())
+                                              selected_left = choices_left()[1],
+                                              selected_right = choices_right()[1])
                      insertUI(selector = glue("#{ns('add')}"),
                               where = 'beforeBegin',
                               ui = ui, immediate = T)
-                     isolate(.mods <- mods())
-                     mods(c(.mods, list(map_pair_select_Server(new_id))))
+                     #isolate(.mods <- mods())
+                     mods(c(mods(), list(map_pair_select_Server(new_id))))
                    })
 
+      #Whenever something is zapped - finally remove the module's {index}-rm input
       observeEvent({
         zapped()
       }, {
@@ -140,13 +118,22 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
         remove_shiny_inputs(glue("{ns(zap_indx)}-rm"), input)
         isolate(.mods <- mods())
         mods(.mods[!zapped()])
+        removing <<- removing - length(zap_indx)
+        if(removing==0L&length(isolate(mods()))==0L) {
+          onFlushed(function(){
+            observeEvent(NULL, {
+              depend(depend() + 1L)
+            }, once = TRUE, ignoreNULL = FALSE, ignoreInit = FALSE)
+          })
+        }
 
-      })
+      }, priority = 3L)
 
       mod_ids <- reactive({
         vapply(mods(), function(x){x[['id']]}, double(1))
       })
       zapped <- reactive({
+        validate(need(!is_empty(mods()), "No Modules"))
         vals <- lapply(mods(), function(x){x[['zap']]()})
         validate(
           need(all(!vapply(vals, is.null, logical(1))), "Some inputs haven't fully registered"),
@@ -157,8 +144,10 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
       left <- reactive({
         isolate(left_choice <- selected_left() %||% choices_left()[1])
         n <- length(left_choice)
-        if(n!=length(mods())|n!=1L) {
-          left_choice[(n+1L):length(mods())] <- ""
+        if(length(mods())==0){
+          left_choice <- NULL
+        } else if (n!=length(mods())|n!=1L) {
+          left_choice[(n+1L):length(mods())] <- isolate(choices_left()[1])
         }
         vals <- mapply(function(x, default){x[['left']]() %empty% default}, x = mods(), default = left_choice, SIMPLIFY = F)
         validate(
@@ -169,11 +158,14 @@ selectMappingServer <- function(id, choices_left = NULL, choices_right = NULL,
       right <- reactive({
         isolate(right_choice <- selected_right() %||% choices_right()[1])
         n <- length(right_choice)
-        if(n!=length(mods())|n!=1L) {
-          right_choice[(n+1L):length(mods())] <- ""
+        if(length(mods())==0){
+          right_choice <- NULL
+        } else if (n!=length(mods())|n!=1L) {
+          right_choice[(n+1L):length(mods())] <- isolate(choices_right()[1])
         }
         vals <- mapply(function(x, default){x[['right']]() %empty% default}, x = mods(), default = right_choice, SIMPLIFY = F)
         validate(
+          need(!is.null(vals), "No inputs available"),
           need(all(!vapply(vals, is.null, logical(1))), "Some inputs on right haven't fully registered")
         )
         unlist(vals)
